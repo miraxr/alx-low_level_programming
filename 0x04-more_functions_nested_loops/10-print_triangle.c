8700c4ae0583fa091b287eb6393391e942b862a0
miraxr <miracleugochukwunextgen@gmail.com>
2022-09-15 20:49:18 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,6 +15,8 @@ void print_triangle(int size)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
 		{
+			for((inc2 = size - inc1); inc2 > 0; inc 2--)
+		{
 			putchar(' ');
 		}
 
@@ -28,6 +30,7 @@ void print_triangle(int size)
 			continue;
 		}
 		putchar('\n');
+		}
 	}
 	putchar('\n');
 }
