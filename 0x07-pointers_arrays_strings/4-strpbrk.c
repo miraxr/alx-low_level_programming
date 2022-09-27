bb977b13cd0d20b0ca08c06f51dffbc0ee8a0329
miraxr <miracleugochukwunextgen@gmail.com>
2022-09-27 15:12:08 -0700

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strbrk - searches a string for any of a set of bytes
+ * _strpbrk - searches a string for any of a set of bytes
  * @s: string to check
  * @accept: string to check against
  *
  * Return: pointer to byte in s that matches or NULL if no match
  */
-char *_strbrk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
